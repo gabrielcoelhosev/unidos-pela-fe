9f7318b37e86cfd5aa3cf7735c1e38d807fc48d8
gabrielcoelhosev <gabrielcoelhosev.dev@gmail.com>
2025-01-18 19:42:55 -0300

    feat: Tirar o Header fixo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,6 @@ function Home(){
             <div className='div1'>
                 <h1>Teste1</h1>
             </div>
-
             <div className='div2'>
                 <h1>Teste2</h1>
             </div>
